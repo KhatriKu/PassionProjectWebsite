4f723b5471ced081790ec4f0749461a3d0c51080
Kushal Khatri <97414683+KhatriKu@users.noreply.github.com>
2025-04-06 18:49:14 -0400

    Updates
    Signed-off-by: Kushal Khatri <97414683+KhatriKu@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-var clicks = 0;
-if(clicks==3){
-    clicks = 1;
-}
-function playPause(btn, Video1) {
-    var video = document.getElementById(Video1);
-    if(video.pause){
-        video.play();
-        btn.innerHTML = "Click To Pause(currently bugged)";
-        //We use the btn.innerhtml to change what it says on the button
-        //use a if then and a boolen with the % comparison for pause
-    } else if(video.play){
-        video.pause();
-        btn.innerHTML = "Click To Play";
-    }
-}
-console.log("//Kushal add different types of videos like webm and ogg so things work now matter the browser!!(and more lines of code)");
+console.log("Kushal add different types of videos like webm and ogg so things work now matter the browser!!(and more lines of code)");
+
+var video = document.getElementById('Video1');  playVideo();
+
+//var video = document.getElementById('myVideo');
 
 function dosomething(){
     document.getElementById("greeting").innerHTML= "Hello and Goodbye";
     
 }
+
 function posture(){
     document.getElementById("posture").innerHTML= "In the video the correcct posture demonstrated is hips open, the passing foots placement is perpendicular to that of the target, with the center of gravity being a bisector of the right angle legs and knees are facing outwards, center of gravity is lowered for power";
 } 
+
+function updateText(elementName, text){
+    document.getElementById(elementName).innerHTML=text;
+}
+
+var video = document.getElementById('Video1');  playVideo();
+        function playVideo() {
+            video.play();
+        }
+
+        function pauseVideo() {
+            video.pause();
+        }
+
+        function forwardVideo() {
+            video.currentTime += 2; // Move forward by 2 seconds
+        }
+
+        function backwardVideo() {
+            video.currentTime -= 2; // Move backward by 2 seconds
+            if (video.currentTime < 2) { 
+                video.currentTime = 0; 
+            }
+        }
